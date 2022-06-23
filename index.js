7ada8551eba93bc4fa564067267e7159b9190a0e
TabithaKoech <tabbyk1718@gmail.com>
2022-06-23 12:17:42 +0300

    fetching the data from the cocktailsDB API

--- index.js
+++ index.js
@@ -20,3 +20,31 @@
 //   };
   
 //   fetchPeople();
+
+let url = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
+fetch(url)
+    .then((response) =>(response.json()))
+    .then((data) => {
+        console.log(data);
+    //     let html = "";
+    //     data.drinks.map((value) => {
+    //         html +=`
+    //         <div class="col-md-4">
+    //             <div class="card" style="width:350px">
+    //                 <img class="card-img-top" src="${value.strDrinkThumb}" alt="Card image" style="width:100%">
+    //                 <div class="card-body">
+    //                   <h4 class="card-title">${value.strDrink}</h4>
+    //                   <p class="card-text">Instructions</p>
+    //                   <p class="card-text">${value.strInstructions}</p>
+    //                   <a href="#" class="btn btn-primary">${value.strGlass}</a>
+    //                 </div>
+    //             </div>
+
+    //         </div>
+    //         `
+    //     document.getElementById("content").innerHTML = html;
+    //     });
+    // })
+    // .catch((error) => {
+    //     console.log(error);
+    // } )
