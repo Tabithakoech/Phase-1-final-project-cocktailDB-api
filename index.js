45da0e04a3b0784388501b12e55e82daafe73d86
TabithaKoech <tabbyk1718@gmail.com>
2022-06-23 09:28:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// const fetchCocktails= async()=>{
+//     const response = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=')
+//     const data = await response.json()
+//     console.log (data);
+//     const results= data.results
+//     // return data
+// }
+// fetchCocktails()
+
+// console.log ('hello')
+
+// const fetchPeople = async () => {
+//     const response = await fetch("https://thecocktaildb.com/api/json/v1/1/search.php?s=");
+//     const data = await response.json();
+//     console.log(data);
+//     const results = data.results;
+  
+//     results.map((person) => renderPerson(person));
+//     console.log(results);
+//   };
+  
+//   fetchPeople();
