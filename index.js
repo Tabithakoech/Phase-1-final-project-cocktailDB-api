92c0348e4853e646a4c638201481355151b2ccb2
TabithaKoech <tabbyk1718@gmail.com>
2022-06-26 15:11:27 +0300

    changed the position of the likes button

--- index.js
+++ index.js
@@ -1,170 +1,9 @@
-// const fetchCocktails= async()=>{
-//     const response = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=')
-//     const data = await response.json()
-//     console.log (data);
-//     const results= data.results
-//     // return data
-// }
-// fetchCocktails()
-
-// console.log ('hello')
-
-// const fetchPeople = async () => {
-//     const response = await fetch("https://thecocktaildb.com/api/json/v1/1/search.php?s=");
-//     const data = await response.json();
-//     console.log(data);
-//     const results = data.results;
-
-//     results.map((person) => renderPerson(person));
-//     console.log(results);
-//   };
-
-//   fetchPeople();
-// document.addEventListener("DOMContentLoaded", function () {
-//     const search = document.querySelector("#search");
-// });
-// let drinkSearch=document.getElementById("search");
-// drinkSearch.addEventListener("keyup", () )
-//     input.addEventListener('change',()=>{
-//         filter=input.value
-//         return filter
-//      } )
-
-
-
-
-
-//   let url2 = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${filter}";
-//   fetch(url2)
-//       .then((response) =>(response.json()))
-//       .then((data) => {
-//           console.log(data);
-          
-//   Document.addEventListener("DOMContentLoaded", function () {
-//     const ourSearch = document.getElementById("search-input");
-//     ourSearch.addEventListener("keyup", () => {
-//         let filter = ourSearch.value;
-//         let html = "";
-//         data.drinks.map((value) => {
-//             if (value.strDrink.toLowerCase().includes(filter.toLowerCase())) {
-//                 html += `
-//                 <div class="col-md-4">
-//                     <div class="card" style="width:350px">
-//                         <img class="card-img-top" src="${value.strDrinkThumb}" alt="Card image" style="width:100%">
-//                         <div class="card-body">
-//                           <h4 class="card-title">${value.strDrink}</h4>
-//                           <p class="card-text">Instructions</p>
-//                           <p class="card-text">${value.strInstructions}</p>
-//                           <a href="#" class="btn btn-primary">${value.strGlass}</a>
-//                         </div>
-//                     </div>
-
-//                 </div>
-
-//                 `
-//             }
-//             document.getElementById("content").innerHTML = html;
-//         });
-//     });
-// } );
-
-
-
-
-// const searchBtn = document.getElementById('search-btn');
-// const cocktailList = document.getElementById('cocktail');
-
-// searchBtn.addEventListener('click', cocktailList);
-
-
-
-
-
-
-// get meal list that matches with the ingredients
-// function getCocktailList() {
-//     let searchInputTxt = document.getElementById('search-input').value.trim();
-//     fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=${searchInputTxt}')
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log (data);
-//             let html = "";
-//             data.drinks.map((value) => {
-//                 html += `
-//                 <div class="col-md-4">
-//                   <div class="card" style="width:350px">
-//                     <img class="card-img-top" src="${value.strDrinkThumb}" alt="Card image" style="width:100%">
-//                     <div class="card-body">
-//                       <h4 class="card-title">${value.strDrink}</h4>
-//                       <p class="card-text">Instructions</p>
-//                       <p class="card-text">${value.strInstructions}</p>
-//                       <a href="#" class="btn btn-primary">${value.strGlass}</a>
-//                     </div>
-//                    </div>
-
-//                 </div>
-//                 `;
-//             });
-//         })
-//           document.getElementById("cocktail").innerHTML = html;
-
-//       // cocktailList.innerHTML = html;
-//     }
-//     document.addEventListener("DOMContentLoaded", function () {
-//       const search = document.querySelector("#search");
-//   });
-//   let drinkSearch=document.getElementById("search");
-//   drinkSearch.addEventListener("keyup", () => {
-//       searchInputTxt.addEventListener('change',()=>{
-//           filter=searchInputTxt.value
-//           return filter
-//        } )
-//       })
-
-
-
-
-
-
-
-// let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-// fetch(url)
-//     .then((response) =>(response.json()))
-//     .then((data) => {
-//         console.log(data);
-//         let html = "";
-//         data.drinks.map((value) => {
-//             html +=`
-// <div class="col-md-4">
-//     <div class="card" style="width:350px">
-//         <img class="card-img-top" src="${value.strDrinkThumb}" alt="Card image" style="width:100%">
-//         <div class="card-body">
-//           <h4 class="card-title">${value.strDrink}</h4>
-//           <p class="card-text">Instructions</p>
-//           <p class="card-text">${value.strInstructions}</p>
-//           <a href="#" class="btn btn-primary">${value.strGlass}</a>
-//         </div>
-//     </div>
-
-// </div>
-//             `
-//         document.getElementById("content").innerHTML = html;
-//         });
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     } )
-
-
-
 "use strict";
+document.addEventListener("DOMContentLoaded", () => {
+})
 
 const cocktail = document.getElementById('get_cocktail');
 const cocktail_container = document.getElementById('cocktail');
-
-
-
-
 cocktail.addEventListener('click', () => {
 	fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 		.then(res => res.json())
@@ -191,7 +30,9 @@ const createCocktail = (cocktail) => {
 				<img src="${cocktail.strDrinkThumb}" >
             </div>
             <div id="seven">
-               <h4>${cocktail.strDrink}</h4>
+               <h4>${cocktail.strDrink}</h4><span><div class="likes-section">
+			   <button id="like-button" class="like"><span id="like-count" class="likeNum">0 likes</span>♥</button>
+			 </div></span>
 				<p><strong>Category:</strong> ${cocktail.strCategory}</p>
 				<p><strong>Glass type:</strong> ${cocktail.strGlass}</p>
 				<p><strong>Type:</strong> ${cocktail.strAlcoholic}</p>
@@ -201,9 +42,7 @@ const createCocktail = (cocktail) => {
 				</ul>
                 <h5>Instructions:</h5>
 				<p>${cocktail.strInstructions}</p>
-				<div class="likes-section">
-                  <button id="like-button" class="like"><span id="like-count" class="likeNum">0 likes</span>♥</button>
-                </div>
+				
 			</div>
 		</div>
 	`;
@@ -218,10 +57,11 @@ let likeClick;
 
 increaseLike = () => {
 	  like++;
-    getLikeCount.innerHTML = `${'like'}`
+    getLikeCount.innerHTML = `${like}`
 
 }
 likeClick	= () => {
 	increaseLike();
 }
 getLike.addEventListener("click", likeClick);
+like.target.style.backgroundColor = "red";
