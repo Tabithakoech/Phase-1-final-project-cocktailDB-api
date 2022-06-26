234917ced525be9f5ad0f3575209c60b2118f297
TabithaKoech <tabbyk1718@gmail.com>
2022-06-26 20:40:24 +0300

    added a like button and a click event listener

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
+	fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+
 })
 
 const cocktail = document.getElementById('get_cocktail');
@@ -30,9 +32,16 @@ const createCocktail = (cocktail) => {
 				<img src="${cocktail.strDrinkThumb}" >
             </div>
             <div id="seven">
-               <h4>${cocktail.strDrink}</h4><span><div class="likes-section">
-			   <button id="like-button" class="like"><span id="like-count" class="likeNum">0 likes</span>♥</button>
-			 </div></span>
+               <h4>${cocktail.strDrink}</h4>
+
+			   <div class="likes"> 
+			      <button id="likeButton">
+			        <i class="fa fa-thumbs-up"></i>
+			      </button>
+				  <input type="number" id="inputLike" value="0" name="">likes</input>
+		       </div>
+			 
+			   
 				<p><strong>Category:</strong> ${cocktail.strCategory}</p>
 				<p><strong>Glass type:</strong> ${cocktail.strGlass}</p>
 				<p><strong>Type:</strong> ${cocktail.strAlcoholic}</p>
@@ -45,23 +54,52 @@ const createCocktail = (cocktail) => {
 				
 			</div>
 		</div>
+
 	`;
+
+	cocktail_container.innerHTML = newInnerHTML;
+	let likebtn = document.querySelector('#likeButton');
+    let inputLike= document.querySelector('#inputLike');
+
+    likebtn.addEventListener('click', () => {
+	inputLike.value = parseInt(inputLike.value) + 1;
+	// inputLike.style.color = "#12ff00";
+})
 	
-cocktail_container.innerHTML = newInnerHTML;
 }
-const getLike=document.querySelector(".like");
-const getLikeCount = document.querySelector(".likeNum");
-let like = 0;
-let increaseLike;
-let likeClick;
 
-increaseLike = () => {
-	  like++;
-    getLikeCount.innerHTML = `${like}`
 
-}
-likeClick	= () => {
-	increaseLike();
-}
-getLike.addEventListener("click", likeClick);
-like.target.style.backgroundColor = "red";
+
+
+
+
+
+
+// const cocktail_footer = document.getElementById('footer-content');
+// 	const newInnerHTML2=`
+// 	div id="rowb" id="footer">
+// 	<footer><p>hhhhh</p></footer>`
+
+// 	cocktail_footer.innerHTML = newInnerHTML;
+
+
+// const getLike=document.querySelector(".like");
+// const getLikeCount = document.querySelector(".likeNum");
+// let integer = 0;
+// getLike.addEventListener('click', () => {
+//     integer += 1
+//     getLikeCount.innerHTML = `${integer} likes`;
+// })
+// let increaseLike;
+// let likeClick;
+
+// increaseLike = () => {
+// 	  like++;
+//     getLikeCount.innerHTML = `${like}`
+
+// }
+// likeClick	= () => {
+// 	increaseLike();
+// }
+// getLike.addEventListener("click", likeClick);
+// like.target.style.backgroundColor = "red";
